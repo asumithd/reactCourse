3e9066a2fa22fcc4f212d4dc4ffc3cd90b292e63
asumithd <sumithdeepan@gmail.com>
2021-07-20 21:16:08 +0530

    frontend

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,34 @@
-import React from 'react'
-import {Container, Navbar,Nav} from 'react-bootstrap';
+import React from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
-    return (
-        <header>
-            <Navbar bg="light" expand="lg" collapseOnSelect>
-  <Container>
-    <Navbar.Brand href="/">Sumith</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="/cart"><i className='fas fa-shopping-cart'>Cart</i></Nav.Link>
-        <Nav.Link href="/login"><i className='fas fa-user'>Login</i></Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-        </header>
-    )
-}
+  return (
+    <header>
+      <Navbar bg="light" expand="lg" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand href="/">Sumith</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart">Cart</i>
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user">Login</i>
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
